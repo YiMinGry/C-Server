2c85d2b1a8186bd96823469a7a0dd3f647b381a2
DESKTOP-N7USNTH\user <ymg9512@gmail.com>
2022-05-26 19:33:04 +0900

    no message

---
+++ websocketTest/websocketTest/MGServer.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Threading.Tasks;
+using WebSocketSharp;
+using WebSocketSharp.Server;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace websocketTest
+{
+    public class MGServer : WebSocketBehavior
+    {
+        private string _suffix;
+        public MGServer() : this(null)
+        {
+
+        }
+        public MGServer(string suffix)
+        {
+            _suffix = suffix ?? String.Empty;
+        }
+        protected override void OnMessage(MessageEventArgs e)
+        {
+            MainWindow win = MainWindow.getWindow;
+
+            var text = e.Data;
+            win.addText("server recv : " + text + "\n");
+            JObject json2 = new JObject();
+            json2 = JObject.Parse(text);
+
+            string _cmd = json2["cmd"].ToString();
+
+            switch (_cmd)
+            {
+                case "userEnter":
+                    JObject retJson = new JObject();
+                    retJson.Add("cmd", "userEnter");
+                    retJson.Add("retMsg", "입장 성공");
+                    retJson.Add("ssid", ID);
+
+                    Sessions.SendTo(retJson.ToString(), ID);
+                    //Sessions.Broadcast(e.Data);
+                    break;
+                case "원하는기능":
+
+                    break;
+            }
+        }
+    }
+}
---
+++ websocketTest/websocketTest/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿//MainWindow.xaml.cs
+using System;
+using System.Windows;
+using WebSocketSharp.Server;
+ 
+namespace websocketTest
+{
+    public partial class MainWindow : Window
+    {
+        private static MainWindow g_Main = null;
+        public static MainWindow getWindow
+        {
+            get
+            {
+                return g_Main;
+            }
+        }
+        public void addText(string text)
+        {
+            this.Dispatcher.Invoke
+            (
+                (Action)(() =>
+                {
+                    string myText = Console.Text;
+                    myText += text;
+                    Console.Text = myText;
+                })
+            );
+        }
+        public MainWindow()
+        {
+            InitializeComponent();
+            g_Main = this;
+        }
+ 
+        private HttpServer webSocketServer = null;
+ 
+        private void Start_Click(object sender, RoutedEventArgs e)
+        {
+            webSocketServer = new HttpServer(0000);
+            webSocketServer.OnGet += (sen, ex) =>
+            {
+                var req = ex.Request;
+                var res = ex.Response;
+ 
+            };
+            webSocketServer.AddWebSocketService<MGServer>("/MGServer");
+            webSocketServer.Start();
+            Console.Text += "서버시작\n";
+        }
+
+        private void Stop_Click(object sender, RoutedEventArgs e)
+        {
+            if (webSocketServer != null)
+                webSocketServer.Stop();
+            webSocketServer = null;
+            Console.Text += "서버종료\n";
+        }
+ 
+    }
+ }
