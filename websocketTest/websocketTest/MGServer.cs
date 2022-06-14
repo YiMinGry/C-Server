0c9064e4520fb574839f13edb5e4899386154f5d
DESKTOP-N7USNTH\user <ymg9512@gmail.com>
2022-06-14 23:24:27 +0900

    서버 모듈 리펙토링, 랭킹 추가

--- websocketTest/websocketTest/MGServer.cs
+++ websocketTest/websocketTest/MGServer.cs
@@ -25,6 +25,11 @@ namespace websocketTest
         int _port = 3306;
         string _database = "userinfo";
         string _infoTable = "info";
+        //string _mg1RankTable = "mg1rank";
+        //string _mg2RankTable = "mg2rank";
+        //string _mg3RankTable = "mg3rank";
+        //string _mg4RankTable = "mg4rank";
+        //string _mg5RankTable = "mg5rank";
 
         string _id = "root";
         string _pw = "root";
@@ -32,39 +37,61 @@ namespace websocketTest
 
         MainWindow win = MainWindow.getWindow;
 
+        //커맨드 리턴
+        public MySqlCommand GetCommand(string _query)
+        {
+            MySqlConnection conn = new MySqlConnection(_connectionAddress);
+            conn.Open();
+
+            MySqlCommand cmd = new MySqlCommand();
+            cmd.Connection = conn;
+            cmd.CommandText = _query;
+            cmd.ExecuteNonQuery();
+            return cmd;
+        }
+        //데이터리더 리턴
+        public MySqlDataReader GetDataReader(string _query)
+        {
+            MySqlConnection conn = new MySqlConnection(_connectionAddress);
+            conn.Open();
+
+            MySqlCommand cmd = new MySqlCommand();
+            cmd.Connection = conn;
+            cmd.CommandText = _query;
+            MySqlDataReader rdr = cmd.ExecuteReader(System.Data.CommandBehavior.CloseConnection);
+
+            return rdr;
+        }
 
         //유저찾기
         private bool FindUserInfo(string _id)
         {
             bool ret = false;
+
             try
             {
-                using (MySqlConnection mysql = new MySqlConnection(_connectionAddress))
+                string _query = string.Format($"SELECT * FROM {_infoTable}");
+
+                MySqlDataReader table = GetDataReader(_query);
+
+                while (table.Read())
                 {
-                    mysql.Open();
-                    string selectQuery = string.Format($"SELECT * FROM {_infoTable}");
-
-                    MySqlCommand command = new MySqlCommand(selectQuery, mysql);
-                    MySqlDataReader table = command.ExecuteReader();
-
-                    while (table.Read())
+                    if (_id.Equals(table["ID"].ToString()))
                     {
-                        if (_id.Equals(table["ID"].ToString()))
-                        {
-                            ret = true;
-                            break;
-                        }
+                        ret = true;
+                        break;
                     }
-
-                    table.Close();
                 }
+
+                table.Close();
             }
             catch (Exception exc)
             {
-                //MessageBox.Show(exc.Message);
+                win.addText("FindUserInfo !!!!!" + exc.Message + "\n");
             }
 
             return ret;
+
         }
         //유저 데이터 받아오기
         private userData GetUserInfo(string _id)
@@ -73,43 +100,38 @@ namespace websocketTest
 
             _info.idx = "";
 
-
             try
             {
-                using (MySqlConnection mysql = new MySqlConnection(_connectionAddress))
+                string _query = string.Format($"SELECT * FROM {_infoTable}");
+
+                MySqlDataReader table = GetDataReader(_query);
+
+                while (table.Read())
                 {
-                    mysql.Open();
-                    string selectQuery = string.Format($"SELECT * FROM {_infoTable}");
-
-                    MySqlCommand command = new MySqlCommand(selectQuery, mysql);
-                    MySqlDataReader table = command.ExecuteReader();
-
-                    while (table.Read())
+                    if (_id.Equals(table["ID"].ToString()))
                     {
-                        if (_id.Equals(table["ID"].ToString()))
-                        {
-                            _info.idx = table["idx"].ToString();
-                            _info.ssID = table["ssID"].ToString();
-                            _info.ID = table["ID"].ToString();
-                            _info.nickName = table["nickName"].ToString();
-                            _info.coin1 = table["coin1"].ToString();
-                            _info.coin2 = table["coin2"].ToString();
+                        _info.idx = table["idx"].ToString();
+                        _info.ssID = table["ssID"].ToString();
+                        _info.ID = table["ID"].ToString();
+                        _info.nickName = table["nickName"].ToString();
+                        _info.coin1 = table["coin1"].ToString();
+                        _info.coin2 = table["coin2"].ToString();
 
-                            return _info;
-                        }
+                        return _info;
                     }
-
-                    table.Close();
                 }
+
+                table.Close();
+
             }
             catch (Exception exc)
             {
-                win.addText("!!!!!" + exc.Message + "\n");
+                win.addText("GetUserInfo !!!!!" + exc.Message + "\n");
             }
 
             return _info;
-        }
 
+        }
         // 유저생성
         public bool UserInsert(JObject _data)
         {
@@ -125,90 +147,64 @@ namespace websocketTest
             {
                 try
                 {
-                    using (MySqlConnection mysql = new MySqlConnection(_connectionAddress))
-                    {
-                        mysql.Open();
-                        string insertQuery = string.Format($"INSERT INTO {_infoTable} (ssID, ID, nickName,coin1, coin2) VALUES ('{_data["ssID"].ToString()}','{_data["ID"].ToString()}','{""}','{1000}','{10}');");
+                    string _query = string.Format($"INSERT INTO {_infoTable} (ssID, ID, nickName,coin1, coin2) VALUES ('{_data["ssID"].ToString()}','{_data["ID"].ToString()}','{""}','{1000}','{10}');");
+                    MySqlCommand command = GetCommand(_query);
 
-                        MySqlCommand command = new MySqlCommand(insertQuery, mysql);
-
-                        if (command.ExecuteNonQuery() != 1)
-                        {
-                            //MessageBox.Show("Failed to insert data.");
-                            ret = false;
-                        }
-
-                        //selectTable();
-
-                        ret = true;
-                    }
+                    ret = true;
                 }
                 catch (Exception exc)
                 {
 
-                    win.addText("!!!!!" + exc.Message + "\n");
+                    win.addText("UserInsert !!!!!" + exc.Message + "\n");
                     ret = false;
                 }
             }
             return ret;
+
         }
         // 유저닉네임 체크
         private bool CheckUserNickName(string _id)
         {
+
             bool ret = false;
             try
             {
-                using (MySqlConnection mysql = new MySqlConnection(_connectionAddress))
+                string _query = string.Format($"SELECT * FROM {_infoTable}");
+
+                MySqlDataReader table = GetDataReader(_query);
+
+                while (table.Read())
                 {
-                    mysql.Open();
-                    string selectQuery = string.Format($"SELECT * FROM {_infoTable}");
-
-                    MySqlCommand command = new MySqlCommand(selectQuery, mysql);
-                    MySqlDataReader table = command.ExecuteReader();
-
-                    while (table.Read())
+                    if (_id.Equals(table["ID"].ToString()) && table["nickName"].ToString() != "")
                     {
-                        if (_id.Equals(table["ID"].ToString()) && table["nickName"].ToString() != "")
-                        {
-                            ret = true;
-                            break;
-                        }
+                        ret = true;
+                        break;
                     }
-
-                    table.Close();
                 }
+
+                table.Close();
             }
             catch (Exception exc)
             {
-                //MessageBox.Show(exc.Message);
+                win.addText("CheckUserNickName !!!!!" + exc.Message + "\n");
             }
 
             return ret;
         }
-
         //닉네임 수정
         private void UserNinameUpdate(string _id, string _nName)
         {
+
             try
             {
+                string _query = string.Format($"UPDATE {_infoTable} SET nickName='{_nName}' WHERE ID='{_id}';");
+                MySqlCommand command = GetCommand(_query);
 
-                using (MySqlConnection mysql = new MySqlConnection(_connectionAddress))
-                {
-                    mysql.Open();
 
-                    string updateQuery = string.Format($"UPDATE {_infoTable}  SET nickName = '{_nName}' WHERE ID={_id};");
-
-                    MySqlCommand command = new MySqlCommand(updateQuery, mysql);
-
-                    if (command.ExecuteNonQuery() != 1)
-                    {
-                    }
-
-                }
             }
             catch (Exception exc)
             {
-                //MessageBox.Show(exc.Message);
+                win.addText("UserNinameUpdate !!!!!" + exc.Message + "\n");
             }
         }
         //세션 업데이트
@@ -216,26 +212,120 @@ namespace websocketTest
         {
             try
             {
+                string _query = string.Format($"UPDATE {_infoTable} SET ssID='{_ssID}' WHERE ID='{_id}';");
 
-                using (MySqlConnection mysql = new MySqlConnection(_connectionAddress))
-                {
-                    mysql.Open();
-
-                    string updateQuery = string.Format($"UPDATE {_infoTable}  SET ssID = '{_ssID}' WHERE ID={_id};");
-
-                    MySqlCommand command = new MySqlCommand(updateQuery, mysql);
-
-                    if (command.ExecuteNonQuery() != 1)
-                    {
-                    }
-
-                }
+                MySqlCommand command = GetCommand(_query);
             }
             catch (Exception exc)
             {
-                //MessageBox.Show(exc.Message);
+                win.addText("UserSSIDUpdate !!!!!" + exc.Message + "\n");
+            }
+
+        }
+
+        //미니게임 랭킹 가져오기
+        private JArray GetMG1TopTRank(string _tableName)
+        {
+
+            JArray ret = new JArray();
+
+            try
+            {
+                string _query = string.Format($"select ID, nickName, Score, dense_rank() over (order by Score desc) as ranking from {_tableName};");
+
+                MySqlDataReader table = GetDataReader(_query);
+
+                JArray rankArr = new JArray();
+
+                while (table.Read())
+                {
+                    if (rankArr.Count < 10)//최대 열개만 나오게
+                    {
+                        JObject rkData = new JObject();
+                        rkData.Add("ID", table["ID"].ToString());
+                        rkData.Add("nickName", table["nickName"].ToString());
+                        rkData.Add("Score", table["Score"].ToString());
+                        rkData.Add("ranking", table["ranking"].ToString());
+
+                        rankArr.Add(rkData);
+                    }
+                }
+
+                ret = rankArr;
+
+                table.Close();
+            }
+            catch (Exception exc)
+            {
+                win.addText("GetMG1TopTRank !!!!!" + exc.Message + "\n");
+            }
+
+            return ret;
+        }
+        //내 미니게임 랭킹 가져오기
+        private JObject GetMG1MyRank(string _tableName, string _id)
+        {
+
+            JObject ret = new JObject();
+
+            try
+            {
+                string _query = string.Format($"SELECT * FROM {_tableName};");
+                MySqlDataReader table = GetDataReader(_query);
+
+                while (table.Read())
+                {
+                    if (_id.Equals(table["ID"].ToString()))
+                    {
+                        ret.Add("msg", "요청한 유저의 랭킹");
+                        ret.Add("ID", table["ID"].ToString());
+                        ret.Add("nickName", table["nickName"].ToString());
+                        ret.Add("Score", table["Score"].ToString());
+                        break;
+                    }
+                }
+
+                table.Close();
+            }
+            catch (Exception exc)
+            {
+                win.addText("GetMG1MyRank !!!!!" + exc.Message + "\n");
+            }
+
+            return ret;
+        }
+        //미니게임 랭킹 추가
+        public void MGRankInsert(string _tableName, JObject _data)
+        {
+            try
+            {
+                string _query = string.Format($"INSERT INTO {_tableName} (ID, nickName, Score) VALUES ('{_data["ID"].ToString()}','{_data["nickName"].ToString()}','{_data["Score"].ToString()}');");
+
+                MySqlCommand command = GetCommand(_query);
+            }
+            catch (Exception exc)
+            {
+
+                win.addText("MGRankInsert !!!!!" + exc.Message + "\n");
+            }
+
+        }
+        //미니게임 랭킹 업데이트
+        private void MGRankUpdate(string _tableName, JObject _data)
+        {
+            try
+            {
+                string _query = string.Format($"UPDATE {_tableName} SET Score='{_data["Score"].ToString()}' WHERE ID='{_data["ID"].ToString()}';");
+
+                MySqlCommand command = GetCommand(_query);
+            }
+            catch (Exception exc)
+            {
+                win.addText("MGRankUpdate !!!!!" + exc.Message + "\n");
             }
         }
+
+
 
 
         private string _suffix;
@@ -262,7 +352,6 @@ namespace websocketTest
             switch (_cmd)
             {
                 case "ssEnter":
-
                     if (FindUserInfo(json2["ID"].ToString()) == true)
                     { //기존 유저인지 체크
 
@@ -345,10 +434,44 @@ namespace websocketTest
                     }
 
                     Sessions.SendTo(retJson.ToString(), ID);
+                    break;
 
+                case "ReadRanking":
+                    JArray rktmp = GetMG1TopTRank(json2["MG_NAME"].ToString());
+                    rktmp.Add(GetMG1MyRank(json2["MG_NAME"].ToString(), json2["ID"].ToString()));
 
+                    Sessions.SendTo(rktmp.ToString(), ID);
 
-                    //Sessions.Broadcast(e.Data);
+                    break;
+                case "UpdateRanking":
+
+                    if (GetMG1MyRank(json2["MG_NAME"].ToString(), json2["ID"].ToString()).Count > 0) //내 정보가 이미 있을때
+                    { //랭킹 점수 업데이트
+                        MGRankUpdate(json2["MG_NAME"].ToString(), json2);
+                    }
+                    else
+                    {//랭킹 추가
+                        MGRankInsert(json2["MG_NAME"].ToString(), json2);
+                    }
+
+                    JArray allRankArr = GetMG1TopTRank(json2["MG_NAME"].ToString());
+                    JObject myRkData = GetMG1MyRank(json2["MG_NAME"].ToString(), json2["ID"].ToString());
+                    int rankIdx = -1;
+
+                    for (int i = 0; i < allRankArr.Count; i++)
+                    {
+                        if (allRankArr[i]["ID"].ToString().Equals(json2["ID"].ToString()))
+                        {
+                            rankIdx = i + 1;
+                        }
+                    }
+
+                    myRkData.Add("ranking", rankIdx);
+
+                    allRankArr.Add(myRkData);
+
+                    Sessions.SendTo(allRankArr.ToString(), ID);
+
                     break;
                 case "원하는기능":
 
